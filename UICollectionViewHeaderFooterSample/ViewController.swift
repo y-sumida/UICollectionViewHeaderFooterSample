805b29808fa1047360c5956ea4f9dd71ca06b36f
Yuki Sumida <y.sumida.0201@gmail.com>
2018-12-18 22:50:58 +0900

    ヘッダ・フッタなしのUICollectionViewを実装

--- UICollectionViewHeaderFooterSample/ViewController.swift
+++ UICollectionViewHeaderFooterSample/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        switch indexPath.section {
+        case 0: cell.backgroundColor = .blue
+        case 1: cell.backgroundColor = .green
+        case 2: cell.backgroundColor = .red
+        default:()
+        }
+        return cell
+    }
 }
 
