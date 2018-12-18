c6b1e388bbf839f5b82e64664684029311887c05
Yuki Sumida <y.sumida.0201@gmail.com>
2018-12-18 22:34:41 +0900

    プロジェクト生成

---
+++ UICollectionViewHeaderFooterSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewHeaderFooterSample
+//
+//  Created by Yuki Sumida on 2018/12/18.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
